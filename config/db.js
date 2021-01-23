dd3ffad3e7f23bfb3e72e883f70e0664cac3b56a
Ankit Tiwari <ankit90499@gmail.com>
2021-01-23 15:36:20 +0530

    - Handled useFindAndModify deprecation

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ async function connectDB() {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: true
+            useFindAndModify: false
         })
         console.log(`MongoDB database connected at host ${conn.connection.host}`)
     } catch (error) {
