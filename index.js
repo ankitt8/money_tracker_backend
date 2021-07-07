08a41197ded418266c83db0334df52da6c7682e6
ankitt8 <ankit90499@gmail.com>
2021-07-07 15:45:04 +0530

    hotfix

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', requestOrigin);
   }
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
 
   // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
