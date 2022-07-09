dea396e1f397dfad544f8cefd361b828de0b81c7
Ankit Tiwari <ankit90499@gmail.com>
2022-07-09 22:22:26 +0530

    chore: prettier changes

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 /* eslint-disable */
-const express = require('express');
+const express = require("express");
 // const { graphqlHTTP } = require('express-graphql');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const { URL } = require('./constant');
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const { URL } = require("./constant");
 // const schema = require('./schema.js');
 // load config
 dotenv.config(getDotEnvConfigOptions());
 
-
-const Transaction = require('./models/Transaction');
-const User = require('./models/User');
+const Transaction = require("./models/Transaction");
+const User = require("./models/User");
 
 const app = express();
 // app.use(graphqlHTTP({
@@ -19,20 +18,25 @@ const app = express();
 // }));
 
 app.use(function (req, res, next) {
-  const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://moneytrackerankit.netlify.app', 'https://issue-37-ui-improvement--moneytrackerankit.netlify.app']
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://moneytrackerankit.netlify.app",
+    "https://issue-37-ui-improvement--moneytrackerankit.netlify.app",
+  ];
   const requestOrigin = req.headers.origin;
   // res.setHeader('Access-Control-Allow-Origin', '*');
   if (allowedOrigins.includes(requestOrigin)) {
-    res.setHeader('Access-Control-Allow-Origin', requestOrigin);
+    res.setHeader("Access-Control-Allow-Origin", requestOrigin);
   }
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST");
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   // for how long to cache the preflight request response
-  res.setHeader('Access-Control-Max-Age', 8640)
+  res.setHeader("Access-Control-Max-Age", 8640);
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   // res.setHeader('Access-Control-Allow-Credentials', true);
@@ -45,54 +49,60 @@ app.use(express.json());
 const checkUserExists = (username) => {
   User.findOne({ username: username }, (err, user) => {
     if (err) {
-      return { userExists: false, error: 'Something broke from our end ):' };
+      return { userExists: false, error: "Something broke from our end ):" };
     } else {
       if (user) return { userExists: true };
-      else return { userExists: false, error: 'Invalid username' }
+      else return { userExists: false, error: "Invalid username" };
     }
   });
-
-}
+};
 // POST method to check the credentials for signin
 app.post(URL.API_URL_SIGNIN, (req, res) => {
   const { username, password } = req.body;
   User.findOne({ username: username }, (err, user) => {
     if (err) {
-      res.status(400).json({ error: 'Something broke from our end ):' })
+      res.status(400).json({ error: "Something broke from our end ):" });
     } else if (user) {
       if (user.password === password) {
         const userId = user._id;
         const username = user.username;
-        res.status(200).json({ success: 'Userlogged in successfully!', userId, username });
+        res
+          .status(200)
+          .json({ success: "Userlogged in successfully!", userId, username });
       } else {
-        res.status(400).json({ error: 'Invalid Username or password' });
+        res.status(400).json({ error: "Invalid Username or password" });
       }
     } else {
-      res.status(400).json({ error: 'User not found ):' });
+      res.status(400).json({ error: "User not found ):" });
     }
   });
-})
+});
 
 app.post(URL.API_URL_SIGNUP, (req, res) => {
   const { username } = req.body;
   User.findOne({ username: username }, (err, user) => {
     if (err) {
-      res.status(400).json({ error: 'Something went wrong' });
+      res.status(400).json({ error: "Something went wrong" });
     } else if (user) {
-      res.status(400).json({ error: 'User Already Exists!' });
+      res.status(400).json({ error: "User Already Exists!" });
     } else {
       const user = new User(req.body);
-      user.save()
+      user
+        .save()
         .then((userSavedDetails) => {
           const { username } = userSavedDetails;
-          res.status(200).json({ ...userSavedDetails, error: '', userId: userSavedDetails._id, username });
+          res.status(200).json({
+            ...userSavedDetails,
+            error: "",
+            userId: userSavedDetails._id,
+            username,
+          });
         })
         .catch((err) => {
-          res.status(400).json({ error: 'Something went wrong' });
-        })
+          res.status(400).json({ error: "Something went wrong" });
+        });
     }
-  })
-
+  });
 });
 
 app.post(URL.API_URL_ADD_TRANSACTION, (req, res) => {
@@ -102,27 +112,24 @@ app.post(URL.API_URL_ADD_TRANSACTION, (req, res) => {
       return res.status(200).json(transactSavedDetails);
     })
     .catch((err) => {
-      return res.status(400).send('Failed To Add transaction');
+      return res.status(400).send("Failed To Add transaction");
     });
 });
 
 app.post(URL.API_URL_GET_TRANSACTION_CATEGORIES, (req, res) => {
-  User.findById(
-    req.body.userId,
-    function callback(err, doc) {
-      if (err) {
-        res.status(404).json({ error: err });
-      } else {
-        res.status(201).json({
-          transactionCategories: {
-            credit: doc.creditTransactionCategories,
-            debit: doc.debitTransactionCategories
-          }
-        })
-      }
+  User.findById(req.body.userId, function callback(err, doc) {
+    if (err) {
+      res.status(404).json({ error: err });
+    } else {
+      res.status(201).json({
+        transactionCategories: {
+          credit: doc.creditTransactionCategories,
+          debit: doc.debitTransactionCategories,
+        },
+      });
     }
-  )
-})
+  });
+});
 
 app.post(URL.API_URL_ADD_CREDIT_TRANSACTION_CATEGORY, (req, res) => {
   const { userId, category } = req.body;
@@ -134,10 +141,12 @@ app.post(URL.API_URL_ADD_CREDIT_TRANSACTION_CATEGORY, (req, res) => {
       if (err) {
         res.status(404).json({ error: err });
       } else {
-        res.status(200).json({ successMsg: `Transaction Category ${category} added successfully` });
+        res.status(200).json({
+          successMsg: `Transaction Category ${category} added successfully`,
+        });
       }
     }
-  )
+  );
 });
 
 app.post(URL.API_URL_ADD_DEBIT_TRANSACTION_CATEGORY, (req, res) => {
@@ -150,10 +159,12 @@ app.post(URL.API_URL_ADD_DEBIT_TRANSACTION_CATEGORY, (req, res) => {
       if (err) {
         res.status(404).json({ error: err });
       } else {
-        res.status(200).json({ successMsg: `Transaction Category ${category} added successfully` });;
+        res.status(200).json({
+          successMsg: `Transaction Category ${category} added successfully`,
+        });
       }
     }
-  )
+  );
 });
 
 app.post(URL.API_URL_DELETE_CREDIT_TRANSACTION_CATEGORY, (req, res) => {
@@ -170,10 +181,12 @@ app.post(URL.API_URL_DELETE_CREDIT_TRANSACTION_CATEGORY, (req, res) => {
       if (err) {
         res.status(404).json({ error: err });
       } else {
-        res.status(200).json({ msg: `Transaction Category ${categories} deleted successfully!` });
+        res.status(200).json({
+          msg: `Transaction Category ${categories} deleted successfully!`,
+        });
       }
     }
-  )
+  );
 });
 
 app.post(URL.API_URL_DELETE_DEBIT_TRANSACTION_CATEGORY, (req, res) => {
@@ -191,10 +204,12 @@ app.post(URL.API_URL_DELETE_DEBIT_TRANSACTION_CATEGORY, (req, res) => {
         console.error(err);
         res.status(404).json({ error: err });
       } else {
-        res.status(200).json({ msg: `Transaction Category ${categories} deleted successfully!` });
+        res.status(200).json({
+          msg: `Transaction Category ${categories} deleted successfully!`,
+        });
       }
     }
-  )
+  );
 });
 
 app.get(URL.API_URL_GET_TRANSACTIONS, (req, res) => {
@@ -260,13 +275,18 @@ app.get(URL.API_URL_GET_TRANSACTIONS, (req, res) => {
 app.post(URL.API_URL_EDIT_TRANSACTION, (req, res) => {
   const updatedTransaction = req.body;
   const id = req.query.id;
-  Transaction.findByIdAndUpdate(id, updatedTransaction, { returnOriginal: false }, (err, result) => {
-    if (err) {
-      res.status(400).send(`Unable to edit transaction with ${id}`);
-    } else {
-      res.status(200).json(result);
+  Transaction.findByIdAndUpdate(
+    id,
+    updatedTransaction,
+    { returnOriginal: false },
+    (err, result) => {
+      if (err) {
+        res.status(400).send(`Unable to edit transaction with ${id}`);
+      } else {
+        res.status(200).json(result);
+      }
     }
-  })
+  );
 });
 
 app.post(URL.API_URL_DELETE_TRANSACTION, (req, res) => {
@@ -277,8 +297,8 @@ app.post(URL.API_URL_DELETE_TRANSACTION, (req, res) => {
     } else {
       res.status(200).json({ transactionId: id });
     }
-  })
-})
+  });
+});
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
@@ -287,15 +307,16 @@ app.listen(port, () => {
 });
 
 function getDotEnvConfigOptions() {
-  const isEnvProduction = process.env.NODE_ENV === 'production';
+  const isEnvProduction = false;
   let dotEnvConfigOptions = {};
+  console.log(isEnvProduction);
   if (isEnvProduction) {
     dotEnvConfigOptions = {
-      path: './config/config-prod.env'
+      path: "./config/config-prod.env",
     };
   } else {
     dotEnvConfigOptions = {
-      path: './config/config-local.env'
+      path: "./config/config-local.env",
     };
   }
   return dotEnvConfigOptions;
